5c14783401bc86af84d9385d55c2e5c72df95545
reegine <regineangelina9@gmail.com>
2025-04-20 00:30:23 +0700

    change the urls for both root and app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -27,7 +27,4 @@ urlpatterns = [
     re_path(r'^auth/', include('djoser.urls.jwt')),
 ]
 
-if settings.DEBUG:
-     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

a559af438bdb48d9e0b137b6a134b7b35739a473
reegine <regineangelina9@gmail.com>
2025-04-20 00:19:25 +0700

    staticfile

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STORAGES = {
